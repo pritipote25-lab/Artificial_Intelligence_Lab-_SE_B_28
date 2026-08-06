6d788cbef2f49ddd0704f19817f7d048674dba2f
pritipote25-lab <priti.pote25@mmit.edu.in>
2026-08-06 09:52:26 +0530

    File 2.py added in the repo

---
+++ 2.py
@@ -0,0 +1,13 @@
+a=input("Enter the first subject:")
+b=input("Enter the second subject:")
+if(a=="maths" and b=="physics"):
+ print("Suggested Career Path: Mechanical Engineering")
+elif(a=="programming" and b=="maths"):
+ print("Suggested Career Path: Computer Engineering")
+elif(a=="biology" and b=="chemistry"):
+ print("Suggested Career Path: Biotechnology")
+elif(a=="circuits" and b=="maths"):
+ print("Suggested Career Path: Electronics Engineering")
+else:
+ print("Explore the subjects for desired career path.")
+
